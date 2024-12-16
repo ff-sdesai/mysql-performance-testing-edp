e055137d554a3023503638109f8145e09f656b49
Abhishek Sharma <abhishek.sharma@uplight.com>
2024-12-16 14:42:25 +0530

    small changes

--- myapi/data_push/views.py
+++ myapi/data_push/views.py
@@ -1,3 +1,4 @@
+import csv
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -12,8 +13,34 @@ def devices_api(request):
         return Response(serializer.data)
 
     if request.method == 'POST':
-        serializer = DeviceSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        if not request.data:  # Check if POST data is empty
+            try:
+                # Read data from local CSV file
+                with open('data_push/data/devices_small.csv', mode='r') as file:
+                    reader = csv.DictReader(file)
+                    data_list = [row for row in reader]
+
+                # Serialize and save data
+                serializer = DeviceSerializer(data=data_list, many=True)
+                if serializer.is_valid():
+                    serializer.save()
+                    return Response(serializer.data, status=status.HTTP_201_CREATED)
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+            except FileNotFoundError:
+                return Response(
+                    {"error": "Local file data.csv not found."},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            except Exception as e:
+                return Response(
+                    {"error": f"An unexpected error occurred: {str(e)}"},
+                    status=status.HTTP_500_INTERNAL_SERVER_ERROR
+                )
+        else:
+            # Process request data normally if it's not empty
+            serializer = DeviceSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
